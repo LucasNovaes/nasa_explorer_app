a52dca760cdf5b3a35482861fb0f3bcaa7daa375
Lucas Novaes <lucas.novaes@spread.com.br>
2023-09-21 13:21:11 -0300

    fix: import errors

--- lib/src/app/app_widget.dart
+++ lib/src/app/app_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../features/detail/detail.dart';
-import '../features/home/home.dart';
+import '../features/astronomy_pictures/presentation/presentation.dart'
+    show HomePage, DetailPage;
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -11,8 +11,8 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       initialRoute: "/",
       routes: {
-        "home": (context) => HomePage(),
-        "detail": (context) => DetailPage(),
+        "home": (context) => const HomePage(),
+        "detail": (context) => const DetailPage(),
       },
     );
   }
